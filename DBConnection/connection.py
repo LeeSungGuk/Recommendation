024f2c7669c3e3779e813fa6a29bd750518f585a
LeeSungGuk <jin3137@gmail.com>
2017-01-13 12:40:19 +0900

    Update connection.py

--- DBConnection/connection.py
+++ DBConnection/connection.py
@@ -11,7 +11,7 @@ from sqlalchemy import PrimaryKeyConstraint
 from sqlalchemy.orm import sessionmaker
 
 server = 'ec2-54-70-249-15.us-west-2.compute.amazonaws.com'
-connection_string = 'mysql+mysqldb://root:dltjdrnr3137@{}:3306/rec'.format(server)
+connection_string = 'mysql+mysqldb://root:*********@{}:3306/rec'.format(server)
 
 redis_pool = redis.ConnectionPool(host=server, port=6379)
 engine = create_engine(connection_string, pool_recycle=3600, encoding='utf-8')
